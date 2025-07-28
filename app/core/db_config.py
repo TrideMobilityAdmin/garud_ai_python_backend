a67f75e80c3a874ae2d2137a2caeca15fb72bc35
Dnadithya <niraja_adithya_dasireddi@@outlook.com>
2025-07-28 16:37:56 +0530

    updated the defects detection

--- app/core/db_config.py
+++ app/core/db_config.py
@@ -1,6 +1,6 @@
 import yaml
 # Load the YAML config file
 def load_config():
-    with open("app\core\config.yaml", "r") as f:
+    with open("app/core/config.yaml", "r") as f:
         config = yaml.safe_load(f)
     return config
